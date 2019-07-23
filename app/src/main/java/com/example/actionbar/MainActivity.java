5b8718e436807560e9553665bf1eb8d9dd055255
richard-remote <clarkeril8082@gmail.com>
2019-07-23 11:21:57 -0400

    Centered Action Bar Setup a basic centered Action Bar

---
+++ app/src/main/java/com/example/actionbar/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.actionbar;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.abs_layout);
+    }
+}
